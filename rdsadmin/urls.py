24894bb58708006705d7af9c98a5e0a17d3616c5
radua <radu.albu@sibs.ro>
2026-07-06 11:53:40 +0300

    Route dyamic files

--- rdsadmin/urls.py
+++ rdsadmin/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.static import serve
 
 from core.admin import admin_site
 
 urlpatterns = [
     path("admin/", admin_site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve media files in both dev and production
+    path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
+]
